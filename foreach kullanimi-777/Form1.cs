f567e6adbf70b4280ab4dc24d57ffa90176bda8a
tugtekinmelis <145451246+tugtekinmelis@users.noreply.github.com>
2024-02-06 12:42:02 +0300

    Add files via upload

---
+++ foreach kullanimi-777/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace foreach_kullanimi_777
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<string> isimler = new List<string>() { "Hayri", "Ufuk", "Ahmet", "Recep" };
+
+            //for 
+            for (int i = 0; i < isimler.Count; i++)
+            {
+                string isim = isimler[i];
+                MessageBox.Show(isim);
+            }
+
+            //foreach 
+            foreach (string isim in isimler)
+            {
+                MessageBox.Show(isim);
+            }
+        }
+    }
+}
